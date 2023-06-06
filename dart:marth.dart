c5c5b286764a4c87e46b9619e8bf9a06f406c549
Ba Long <133968620+lebalong123@users.noreply.github.com>
2023-06-06 12:02:27 +0700

    Update dart:marth.dart

--- dart:marth.dart
+++ dart:marth.dart
@@ -1,66 +1,65 @@
 # dart:math
-```
 import 'dart:math';
 
 // test1
-/*main(List<String> args) {
+main(List<String> args) {
   int i = 2;
   print(i * 5);
   print(i ^ 2);
-}*/
+}
 // test2
-/*main(List<String> args) {
+main(List<String> args) {
   print(sqrt(16));
   print(cos(60 * pi / 180));
   var numbers = [5, 1, 9, 3, 7];
   print(numbers.reduce(max));
-}*/
+}
 //test3
-/*main(List<String> args) {
+main(List<String> args) {
   Random random = Random();
   for (int i = 0; i < 100; i++) {
     int randomNum = random.nextInt(10) + 1;
     print('$randomNum');
   }
-}*/
+}
 //Generate Random Numbers In Dart
-/*void main(List<String> args) {
+void main(List<String> args) {
   Random random = new Random();
   int randomNumber = random.nextInt(10);
   print("Generated Random Number Between 0 to 9: $randomNumber");
 
   int randomNumber2 = random.nextInt(10) + 1;
   print("Generated Random Number Between 1 to 10: $randomNumber2");
-}*/
+}
 //Generate Random Number Between Any Number
-/*main(List<String> args) {
+main(List<String> args) {
   int min = 10;
   int max = 20;
   int random = min + Random().nextInt((max + 1) - min);
   print(random)
-}*/
+}
 // test 4
-/* main(List<String> args) {
+ main(List<String> args) {
   int min = 10;
   int max = 20;
   for (int i = 0; i < 100; i++) {
     int random = min + Random().nextInt((max + 1) - min);
     print(random);
   }
-}*/
+}
 //Generate Random Boolean And Double Values
-/*void main(List<String> args) {
+void main(List<String> args) {
   bool result = Random().nextBool();
   double results = Random().nextDouble();
   print("$result and $results");
-}*/
+}
 // Generate a List Of Random Numbers In Dart
-/*main(List<String> args) {
+main(List<String> args) {
   List<int> randomList = List.generate(10, (_) => Random().nextInt(100) - 1);
   print("Luky number today is :$randomList");
-}*/
+}
 //Math In Dart
-/*main(List<String> args) {
+main(List<String> args) {
   int num1 = 25;
   int num2 = 4;
   num powernum = pow(num1, num2);
@@ -72,7 +71,7 @@ import 'dart:math';
   print("Maximum is $maxnum");
   print("Minimum is $minnum");
   print("Square root is num 1 = $squareroot1 and num 2 =  $squareroot2");
-}*/
+}
 // test 5
 void main(List<String> args) {
   print(pi);
@@ -109,4 +108,4 @@ int gcd(int a, int b) {
   }
   return a;
 }
-```
+
